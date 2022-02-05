9766990d0861e7acd201e4457c6bfb38e9691cc8
AgustinPalmaM <palma.agustin7@gmail.com>
2022-02-05 20:03:49 -0300

    added functionality to each button  to create 8 16 32 64 divs inside main-container

--- script.js
+++ script.js
@@ -1,78 +1,101 @@
-// var smallDivs = document.createElement('div')
-// mainContainer.appendChild(smallDivs)
-// smallDivs.classList.add('small-divs')
-// var smallDivsTwo = document.createElement('div')
-// mainContainer.appendChild(smallDivsTwo)
-// smallDivsTwo.classList.add('small-divs-two')
-
 const mainContainer = document.querySelector('#main-container')
-function agregarDivs(height, width, size) {
-    let maxWidth = width * size
-    let maxHeight = height * size
-    for (let i = 0; i < (height * width); i++) {
+const buttonColor =  document.querySelector('#muestrario')
+const buttonBorrarTodo = document.querySelector('#boton-borrar-todo')
+const buttonBorrar = document.querySelector('#boton-borrar')
+const buttonColorear = document.querySelector('#colorear')
+const buttonEightSquares = document.querySelector('#eight')
+const buttonSixteenSquares = document.querySelector('#sixteen')
+const buttonThirtyTwoSquares = document.querySelector('#thirtyTwo')
+const buttonSixtyFourSquares = document.querySelector('#sixtyFour')
+
+function agregarDivs(numSquares) {
+    let size = (mainContainer.clientWidth) / numSquares;
+    for (let i = 0; i < (numSquares * numSquares); i++) {
         var smallDivs = document.createElement('div')
         mainContainer.appendChild(smallDivs)
-        smallDivs.style.cssText = `width: ${size}px; height: ${size}px; `;
-        mainContainer.setAttribute('style', `max-width: ${maxWidth}px; max-height: ${maxHeight}px`)
-        
-        
+        smallDivs.style.cssText = `width: ${size}px; height: ${size}px; `;   
     }
+}
 
+function eliminarDivs() {
+    while (mainContainer.firstChild) {
+        mainContainer.removeChild(mainContainer.firstChild)
+    }
+}
+
+
+buttonEightSquares.addEventListener('click', eliminarDivs)
+buttonEightSquares.addEventListener('click', () => {
+    agregarDivs(8)
+})
+
+buttonSixteenSquares.addEventListener('click', eliminarDivs)
+buttonSixteenSquares.addEventListener('click', () => {
+    agregarDivs(16)
+})
+
+buttonThirtyTwoSquares.addEventListener('click', eliminarDivs)
+buttonThirtyTwoSquares.addEventListener('click', () => {
+    agregarDivs(32)
+})
+
+buttonSixtyFourSquares.addEventListener('click', eliminarDivs)
+buttonSixtyFourSquares.addEventListener('click', () => {
+    agregarDivs(64)
+})
+
+
+
+function colorear() {
     mainContainer.childNodes.forEach((e) => {
         e.addEventListener('mouseenter', function () {
             e.style.backgroundColor = buttonColor.value
         })
-    })
-    
-    const button = document.querySelector('#boton-borrar')
-    mainContainer.childNodes.forEach((a) => {
-        button.addEventListener('click', function () {
-            a.style.backgroundColor = 'white'
-        })
-    })
-    
-    const buttonColor =  document.querySelector('#muestrario')
-    console.log(buttonColor.value)
-
+    })    
 }
 
-agregarDivs(32,32,15)
+buttonColorear.addEventListener('click', colorear)
+
+function borrador() {
+    mainContainer.childNodes.forEach((d) => {
+        d.addEventListener('mouseenter', () => {
+            d.style.backgroundColor = 'white'
+        })
+    })
+}
+buttonBorrar.addEventListener('click', borrador)
+
+function borrarTodo() {
+    allSmallDivs = mainContainer.querySelectorAll('div');
+    allSmallDivs.forEach(div => div.style.backgroundColor = 'white')
+}
+buttonBorrarTodo.addEventListener('click', borrarTodo)
 
 
 
+// buttonEightSquares.addEventListener('click', agregarDivs(8))
+// buttonEightSquares.addEventListener('click', agregarDivs(16))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// window.onload=function()
-// {
-//     var i=0;
-//     var j=0;
-
-//     for (i=1; i<=300; i++)
-//     {
-        
-//             var d=document.createElement("div");
-//                 mainContainer.appendChild(d);
-//                 d.className="black";
-        
+// const pruebaParent = document.querySelector("#prueba-parent")
+// const pruebaHijo = document.querySelector('#prueba-hijo')
+// pruebaParent.childNodes.forEach((x) => {
+//     if (x.nodeName === 'DIV') {
+//         x.style.cssText = 'background-color: black; height: 0px; width: 0px'
 //     }
-// }
+    
+// })
+
+// console.log(pruebaParent.childNodes);
+
+
+
+
+
+
+
+
+
+
+
+
